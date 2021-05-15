39164c28074c7dc74f428ea9e220de2e0b871ef0
MarshallPeng <marshall.s.peng.20@dartmouth.edu>
2021-05-15 13:32:34 -0500

    Connected GPS and IMU sensors

---
+++ test/manual/imu_sensor_test.py
@@ -0,0 +1,33 @@
+import time
+
+import serial
+import adafruit_bno055
+uart = serial.Serial("/dev/serial0")
+sensor = adafruit_bno055.BNO055_UART(uart)
+
+last_val = 0xFFFF
+
+
+def temperature():
+    global last_val  # pylint: disable=global-statement
+    result = sensor.temperature
+    if abs(result - last_val) == 128:
+        result = sensor.temperature
+        if abs(result - last_val) == 128:
+            return 0b00111111 & result
+    last_val = result
+    return result
+
+
+while True:
+    print("Temperature: {} degrees C".format(temperature()))
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print("Euler angle: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+    print("Gravity (m/s^2): {}".format(sensor.gravity))
+    print()
+
+    time.sleep(1)
